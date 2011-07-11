965fc1bf4d9563159a33f818495ccf181b95e81c
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-07-11 15:32:24 +0800

    basic auth

--- app/controllers/transaction_log_controller.rb
+++ app/controllers/transaction_log_controller.rb
@@ -1,5 +1,7 @@
 class TransactionLogController < ApplicationController
   
+  before_filter :authenticate
+  
   def index
     
   end
@@ -8,4 +10,13 @@ class TransactionLogController < ApplicationController
     path_to_file = LOGFILE_PATH
     @file_contents = IO.readlines(path_to_file)[-40..-1]
   end
+  
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "test" && password == "pass"
+    end
+  end
+  
 end
